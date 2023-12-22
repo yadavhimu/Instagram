e92b6b9d3e4e9363f70a3914572195d111b7dc03
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2023-12-22 20:24:43 +0530

    page work

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,3 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+mongoose.connect()
