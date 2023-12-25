6ac2c8f5510abf6c6f53a57bd5378c12782744d1
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2023-12-25 18:43:42 +0530

    file updated

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+
+passport.use(new localStrategy(userModel.authenticate()));
 
 router.get('/', function(req, res) {
   res.render('index', {footer: false});
@@ -27,6 +32,21 @@ router.get('/edit', function(req, res) {
 
 router.get('/upload', function(req, res) {
   res.render('upload', {footer: true});
+});
+
+router.post("/register", function(req, res, next){
+  const userData = new userModel({
+    username: req.body.username,
+    name:req.body.name,
+    email:req.body.email
+  });
+
+  userModel.register(userData, req.body.password)
+  .then(function(){
+    passport.authenticate("local")(req, res, function (){
+      res.redirect("/profile");
+    })
+  })
 });
 
 module.exports = router;
