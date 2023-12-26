240ce1f018567912065f9831f487d9ce22759040
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2023-12-26 21:44:55 +0530

    file updated

--- routes/index.js
+++ routes/index.js
@@ -14,23 +14,23 @@ router.get('/login', function(req, res) {
   res.render('login', {footer: false});
 });
 
-router.get('/feed', function(req, res) {
+router.get('/feed', isLoggedIn, function(req, res) {
   res.render('feed', {footer: true});
 });
 
-router.get('/profile', function(req, res) {
+router.get('/profile', isLoggedIn, function(req, res) {
   res.render('profile', {footer: true});
 });
 
-router.get('/search', function(req, res) {
+router.get('/search', isLoggedIn, function(req, res) {
   res.render('search', {footer: true});
 });
 
-router.get('/edit', function(req, res) {
+router.get('/edit', isLoggedIn, function(req, res) {
   res.render('edit', {footer: true});
 });
 
-router.get('/upload', function(req, res) {
+router.get('/upload', isLoggedIn, function(req, res) {
   res.render('upload', {footer: true});
 });
 
@@ -54,5 +54,17 @@ router.post("/login", passport.authenticate("local",{
   failureRedirect:"/login"
 }),function(req,res){
 });
+
+router.post("/logout",function (req, res, next) {
+  req.logout(function(err){
+    if (err) {return next (err);}
+    res.redirect('/')
+  });
+});
+
+ function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()) return next();
+  res.redirect("/login")
+ }
 
 module.exports = router;
