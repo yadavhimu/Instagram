ecbf9b5e237e6877038cfe9ecb1cea313fef15d4
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2023-12-28 15:47:47 +0530

    multer file added

--- routes/multer.js
+++ routes/multer.js
@@ -8,9 +8,9 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
       const unique = uuidv4();
-      cb(null,unique + path.extname(file.originalname));
-    }
-  })
+      cb(null, unique + path.extname(file.originalname));
+    },
+  });
   
   const upload = multer({ storage: storage });
   module.exports = upload;
