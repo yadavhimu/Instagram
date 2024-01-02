056160df4c2ee14b0e216f74e0458a9eedbbe6f1
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-01-02 21:14:29 +0530

    profile page updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins:[],
   
-}
+};
 
